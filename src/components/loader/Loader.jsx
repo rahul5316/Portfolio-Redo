65edcacf67a22ecf7ddeec51c8d02aa62cc28e0d
rsinha01 <rsinha01@students.cs.ubc.ca>
2023-09-24 21:09:34 -0700

    changed background color

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,4 +1,4 @@
-import '../../loader.css';
+import '../../loader.scss';
 import $ from 'jquery';
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
